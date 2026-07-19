a3cb39840f9e51cdd454aa7d99cfabb67c16ead0
zebz213 <zebz213@gmail.com>
2026-07-19 13:20:46 -0400

    Add files via upload

---
+++ module/service.sh
@@ -0,0 +1,26 @@
+#!/system/bin/sh
+MODDIR=${0%/*}
+. "$MODDIR/utils.sh"
+
+run() {
+	until [ "$(getprop sys.boot_completed)" = 1 ]; do sleep 1; done
+	until [ -d "/sdcard/Android" ]; do sleep 1; done
+
+	while
+		BASEPATH=$(get_basepath)
+		SVCL=$?
+		[ $SVCL = 20 ]
+	do sleep 2; done
+
+	if [ $SVCL != 0 ]; then
+		ch_desc_err "App not installed"
+		return
+	fi
+	sleep 4
+
+	mount_rv "$BASEPATH"
+}
+
+if [ ! -f "$MODDIR/disabled_by_action" ]; then
+	run
+fi
